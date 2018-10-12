b77e41bdfccd73129adda79dd3a50c2496dd8c8a
JEN-YC <gn9635201@gmail.com>
2018-10-12 14:11:07 +0800

    lack news_api_spec

--- test.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'lib/news_api'
-key = '3798d8d9b44744c3969cf344ce02df14'
-news =CodePraise::NewsAPI.new(key)
-n = news.get_news('', '2018-10-9', '2018-10-10', 'cnn')
-puts n[2].author
