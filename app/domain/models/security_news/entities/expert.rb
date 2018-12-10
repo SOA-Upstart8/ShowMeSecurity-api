3e8b346b80829381b9ea3adcc23a7165b5436dd3
JEN-YC <gn9635201@gmail.com>
2018-12-10 23:35:15 +0800

    update expert user_id

--- app/domain/models/security_news/entities/expert.rb
+++ app/domain/models/security_news/entities/expert.rb
@@ -8,7 +8,7 @@ module SMS
     class Expert < Dry::Struct
       include Dry::Types.module
       attribute :id, Integer.optional
-      attribute :user_id, Strict::Integer
+      attribute :user_id, Coercible::String
       attribute :image, Strict::String
       attribute :follower_count, Strict::Integer
       attribute :name, Strict::String
