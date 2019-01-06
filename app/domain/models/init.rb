a29a88e930c2d9f6582d0ef14d0496bb9eb55e77
JEN-YC <gn9635201@gmail.com>
2019-01-06 15:34:16 +0800

    final version

--- app/domain/models/init.rb
+++ app/domain/models/init.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-folders = %w[security_categories security_CVEs]
+folders = %w[security_owasp security_CVEs]
 folders.each do |folder|
   require_relative "#{folder}/init.rb"
 end
